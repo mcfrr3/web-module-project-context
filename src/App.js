ecd061eaad55c25399e100091e8cd1a17fe2869a
Mike Ferrete <michael.ferrete@gmail.com>
2022-04-21 09:49:43 -0800

    completed step 4 - consuming data with ProductContext (has errors)

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
 				{/* Routes */}
 				<Route exact path="/">
-					<Products products={products} addItem={addItem} />
+					<Products />
 				</Route>
 
 				<Route path="/cart">
